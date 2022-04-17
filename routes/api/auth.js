41db3aa0669fe0e7a5118a5f49b8e7864c3b5179
Amit Kharod <khaordcloud1@gmail.com>
2022-04-17 13:07:05 +0530

    Added routes for registering and authentication of student

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const Student = require('../../models/Student')
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Private
-router.get('/', (req, res) => {
-    res.send('Auth route')
-})
+router.get('/', auth, async (req, res) => {
+  try {
+      const student = await Student.findById(req.student.id).select('-password');
+      res.json(student);
+  } catch (err) {
+      console.error(err.toString())
+      res.status(500).send('Server error')
+  }
+}); 
 
 module.exports = router;
