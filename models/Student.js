e972e6006004060bafbb00d56423be1be39b488d
Amit Kharod <khaordcloud1@gmail.com>
2022-04-17 12:36:52 +0530

    added schema models

---
+++ models/Student.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  registration_no: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('student', StudentSchema);
